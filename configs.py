5576318bf0ffa0ed19e49938ca143b1c9ffae265
tongjinle123 <lancertong@live.com>
2018-07-26 16:16:19 +0800

    init commit

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 class Config(object):
     raw_folder = 'raw/'
     raw_file = 'raw/df.json'
-
+    middle_folder = 'middle/'
 
--- to_df.py
+++ to_df.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
 
 with open('raw/corpus.txt') as reader:
     data = reader.readlines()
@@ -20,4 +22,13 @@ ct = [i if i != '' else None for i in content]
 ctt = [i if i != '' else None for i in contenttitle]
 df2 = pd.DataFrame({'text': ct,'title':ctt})
 df2 = df2.dropna().drop_duplicates()
-df2.to_json('raw/df.json')
+
+train, test = train_test_split(df2, test_size=0.1)
+test, dev = train_test_split(test, test_size=0.5)
+
+import os
+os.mkdir('middle/')
+
+train.to_json('middle/train.json')
+test.to_json('middle/test.json')
+dev.to_json('middle/dev.json')

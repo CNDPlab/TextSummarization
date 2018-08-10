d080498832abc3c9e9e468086ffb0dff96056ad1
tongjinle123 <lancertong@live.com>
2018-08-10 15:28:09 +0800

    fix bug

--- Trainner/trainner.py
+++ Trainner/trainner.py
@@ -18,7 +18,7 @@ class Trainner(object):
     def train(self, model, optimizer, loss_func, score_func, train_loader, dev_loader, teacher_forcing_ratio):
         #TODO add model resume func
         os.mkdir(self.args.tensorboard_root+self.init_time+'/')
-        self.summary_writer = SummaryWriter(args.tensorboard_root+self.init_time+'/')
+        self.summary_writer = SummaryWriter(self.args.tensorboard_root+self.init_time+'/')
         for epoch in range(self.args.epochs):
             self._train_epoch(model, optimizer, loss_func, score_func, train_loader, dev_loader, teacher_forcing_ratio)
             self.global_epoch += 1

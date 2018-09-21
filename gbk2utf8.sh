45f73c3382afa90ef3b520835b949aad96950543
tongjinle123 <lancertong@live.com>
2018-09-21 14:16:31 +0800

    add restrict self attention

--- gbk2utf8.sh
+++ gbk2utf8.sh
@@ -1,1 +1,1 @@
-cat Datas/Sougou/news_sohusite_xml.dat | iconv -f gb18030 -t utf-8 -c | grep -e "<content>" -e "<contenttitle>"  > Datas/Sougou/corpus.txt
+cat Datas/Sougou/news_sohusite_xml.dat | iconv -f gb18030 -t utf-8 -c | grep -e "<content>" -e "<contenttitle>"  > Datas/raw/corpus.txt
